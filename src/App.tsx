3ccc2848da924a6d8c14241e58f3abc35877077d
hainh-3364 <nguyen.hoang.hai-b@sun-asterisk.com>
2024-03-03 18:10:55 +0900

    [React] Task#1: Create Prefecture Checkboxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect,} from "react";
 
-function App() {
+type prefectureProps = {
+  prefCode: string;
+  prefName: string;
+};
+
+export default function App() {
+  const [prefecture, setPrefecture] = useState<prefectureProps[]>([]);
+  const [isChecked, setIsChecked] = useState(false);
+  // const xApiKey: string = (process.env.XAPIKEY as string);
+  const headers: HeadersInit = {
+    'Content-Type': 'application/json',
+    'X-API-KEY': '2DKYRvyCSG8i2eJp0W9IhL2oc6Fsl2Gmjlj6y6CC'
+  }
+  useEffect(() => {
+    const api = async () => {
+      const dataPrefecture = await fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
+        method: "GET",
+        headers: headers
+      });
+      const jsonData = await dataPrefecture.json();
+      setPrefecture(jsonData.result);
+    };
+    api();
+  }, []);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked); // Toggle checkbox state
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>
+        {prefecture.map((value) => {
+          return (
+            <div>
+              <input type="checkbox" id={value.prefCode} name={value.prefName} 
+              value={value.prefCode} checked={isChecked} onChange={handleCheckboxChange}/>
+              <div>{value.prefName}</div>
+            </div>
+          );
+        })}
+      </h1>
       </header>
     </div>
   );
 }
-
-export default App;
